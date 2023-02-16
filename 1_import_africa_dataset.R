1449bc9b09a5e3c72a8a8445f1fb0f92cec1b205
Ritika Singh <88920473+ritikasingh95@users.noreply.github.com>
2023-02-16 16:03:00 +0530

    Update file path

--- 1_import_africa_dataset.R
+++ 1_import_africa_dataset.R
@@ -1,6 +1,6 @@
 #1: Get country dataset
 
 #import africa dataset 
-setwd("C:/Users/manoj/Documents/CTech/Project/AfricaRep")  # set your working dir
+setwd("C:/Users/Documents/CTech/Project/AfricaRep")  # set your working dir
 nigdata = foreign::read.spss(paste("C:/Users/manoj/Documents/CTech/Project/dhs/nigeria/NGKR6AFL.SAV", sep = ""), 
                              to.data.frame = T)  # read data
