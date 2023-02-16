b2446f5a4d4c11586f1c689a3740a395e6035d56
Ritika Singh <88920473+ritikasingh95@users.noreply.github.com>
2023-02-16 16:03:17 +0530

    Update file

--- 1_import_africa_dataset.R
+++ 1_import_africa_dataset.R
@@ -2,5 +2,6 @@
 
 #import africa dataset 
 setwd("C:/Users/Documents/CTech/Project/AfricaRep")  # set your working dir
-nigdata = foreign::read.spss(paste("C:/Users/manoj/Documents/CTech/Project/dhs/nigeria/NGKR6AFL.SAV", sep = ""), 
+nigdata = foreign::read.spss(paste("C:/Users
+/Documents/CTech/Project/dhs/nigeria/NGKR6AFL.SAV", sep = ""), 
                              to.data.frame = T)  # read data
